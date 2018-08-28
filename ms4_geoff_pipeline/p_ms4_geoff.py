7bce4fa33ddce17f6ad2cb1519de818dfbf1d254
GeoffBarrett <geoffrey.m.barrett@gmail.com>
2018-08-28 15:32:51 -0400

    attempting to fix None and null problems

--- ms4_geoff_pipeline/p_ms4_geoff.py
+++ ms4_geoff_pipeline/p_ms4_geoff.py
@@ -63,7 +63,7 @@ def sort_dataset(*, raw_fname=None, pre_fname=None, geom_fname=None, params_fnam
     # if you do not provide an input, it will set the value as an empty string via mountainlab
 
     # find a more pythonic way to do this
-    if raw_fname == '':
+    '''if raw_fname == '':
         raw_fname = None
 
     if pre_out_fname == '':
@@ -82,12 +82,13 @@ def sort_dataset(*, raw_fname=None, pre_fname=None, geom_fname=None, params_fnam
         params_fname = None
 
     if firings_out == '':
-        firings_out = None
+        firings_out = None'''
 
     if raw_fname is None and pre_fname is None:
         raise Exception('You must input a raw_fname or a pre_fname!')
 
-    if raw_fname is not None and pre_fname is not None:
+    # if raw_fname is not None and pre_fname is not None:
+    if raw_fname is not None and pre_fname == '':
         raise Exception('You defined both the raw_fname and the pre_fname, can only use one!')
 
     params = {'freq_min': freq_min,
